7ec60e490d733319a2b95a01a57eaed56a84f364
ITSMijaiL <ITSMijaiL@users.noreply.github.com>
2021-05-14 10:54:53 -0400

    Update phase3-BIOS.sh
    Actually added the packages installation + start xfce4 on boot

--- 2021.05.01/phase3-BIOS.sh
+++ 2021.05.01/phase3-BIOS.sh
@@ -2,4 +2,25 @@
 # - EXITTING CHROOT USING "exit"
 # - UNMOUNTING /MNT USING "umount -R /mnt"
 # - REBOOTING WITHOUT THE INSTALLATION MEDIA USING "reboot"
+# - BOOTING INTO THE NEW SYSTEM
 
+#(THIS FILE IS COMPLETELY OPTIONAL)
+
+#Make user
+echo "Input username:"
+read username_useradd
+useradd $username_useradd -m -G wheel
+
+#Download/install packages
+pacman -S --noconfirm nano
+pacman -S --noconfirm xorg
+pacman -S --noconfirm linux-headers base-devel
+pacman -S --noconfirm xfce4
+
+pacman -S --noconfirm firefox
+pacman -S --noconfirm sudo
+
+#Make xinitrc so that xfce4 executes on boot
+cat > /home/$username_useradd/.xinitrc << "EOF"
+exec startxfce4
+EOF
